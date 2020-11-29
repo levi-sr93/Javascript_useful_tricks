e6cde7a79e0cb06fb622e7075e71dbfdc2e4d935
Levi Santos <levi-sr@outlook.com>
2020-11-29 00:17:50 -0300

    Get RANDOM element from array / Math.random

---
+++ 2-working_with_arrays/get_random_element_from_array.js
@@ -0,0 +1,7 @@
+//Get Random element from array
+let ages = [12, 32, 93, 33, 28, 58];
+
+Math.round(Math.random() * ages.length);
+
+//get a random from index/element of the array
+ages[Math.round(Math.random() * ages.length)];
